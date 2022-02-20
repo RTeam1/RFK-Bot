500f1a6acdcd723200b72424e6a382c59fe70929
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2022-02-20 22:45:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,129 @@
+// Bismillahirrahmanirrahim
+// thank you to ALLAH Subhanahu wata'ala
+// thank you to Rey Alfarizi as Leader RFK Team
+// thank you to Arya Kings as Leader Companion RFK
+// thank you to RFK-TEAM
+// thank you to Real Fams Killer
+// thank you to Alip as Litrhap
+// thank you to RlxFly / Parell
+// thank you to Nurutomo as wabot-aq
+// thank you to ariffb as stikerinbot
+// thank you to Bochil Gaming
+// thank you to Dawnfrost
+// thank you to MFarelS as MagerNulis
+// thank you to ALL Bot creator
+// and thanks you to who support my Bot
+
+/* 
+Note : Bagi yang merasa kode Anda ada di script ini...
+Namun nama Anda tidak disebutkan di config.js ini
+Saya minta maaf sebesar-besarnya, 
+dan Silahkan hubungi Saya di WhatsApp 
+supaya saya menambahkan nama Anda disini
+
+>>> DILARANG KERAS MENGHAPUS THANKS TO <<<
+>>> BOLEH MENAMBAHKAN, JANGAN MENGHILANGKAN <<<
+*/
+
+global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
+
+//Berbagai itu indah @Parel / Rxfly
+global.owner = ['6289677763976', '+7 922 481-65-16', '+62 838-2007-3017'] // Put your number here
+global.mods = ['2'] // Want some help?
+global.prems = ['1'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  dhnjing: 'https://dhnjing.xyz',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey' (apikey kalian)
+  'https://api.xteam.xyz': 'expired',
+  'https://api.lolhuman.xyz': 'rey2k21',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+//global.packname = 'I hope you\'re fine'
+//global.author = 'LitRHap'
+global.packname = './RyuuZeyy\n\n\n./RyuuTod\n\n\n\n\nhttps://wa.me/message/MJKHC7377YLDA1\n\n\n\n\nwa.me/62896-7776-3976\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.author = ''
+
+// silakan di ganti klo mau
+global.wait = '_*tunggu..*_'
+global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
+global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
+global.eror = '_*Server Error*_'
+
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+
+// tingkat kesulitan, semakin tinggi semakin susah
+global.multiplier = 36 // The higher, The harder levelup
+
+//*****************PEMBATAS*********************
+// JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
+//global.prefa = ['','Z','!','.','🐦','🐤','🗿']
+//global.prefa = ['','Z','!','.','🐦','🐤','🗿']
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
+
+//
+const mumet = require("moment-timezone")
+const time = mumet().tz('Asia/Jakarta').format("HH:mm:ss DD:MM:YYYY")
+//const cron = require('node-cron')
+//const { exec, spawn } = require("child_process")
+
+sukses = 'Suksesss...'
+
+// Mau Apa Lu? Mau ubah apikey? Awokaaokaowk Bocil
+Lolkey = 'rey2k21'
+LolKey = 'rey2k21'
+lolkey = 'rey2k21'
+lolKey = 'rey2k21'
+
+
+footer = '\n\n\n@ＪＥＭ － Ｂｏｔ'
+namabot = 'ＪＥＭ － Ｂｏｔ'
+bc = namabot
+//bc = 'ＪＥＭ － Ｂｏｔ'
+namaowner = 'RyuuZeyy'
+ceco = 'Ganteng 🗿'
+let tum = fs.readFileSync('./gambar1.jpeg')
+let tum1 = fs.readFileSync('./gambar1.jpeg')
+let tum2 = fs.readFileSync('./gambar2.jpeg')
+let tum3 = fs.readFileSync('./gambar3.jpeg')
+global.thumb = tum
+global.thumb1 = tum
+global.thumb2 = tum2
+global.thumb3 = tum3
+run = 'tes'
+gc1 = 'https://chat.whatsapp.com/FJkbQjpewltErmQRwzC9rj'
+gc2 = 'https://chat.whatsapp.com/HTcmEqjQeu1GMN2xN210QJ'
+linkf = gc1
+wm_rey = 'https://raw.githubusercontent.com/RTeam1/database/'
+namaig = 'https://instagram.com/bah.raid'
+namagh = 'https://github.com/RTeam1'
+kasihcaption = `support terus ${namaig}\ndan \n${namagh}`
+browsername = './RyuuTodd'
+titler = '𝙍𝙚𝙖𝙡 𝙁𝙖𝙢𝙨 𝙆𝙞𝙡𝙡𝙚𝙧'//'https://ryuuzeyy.xyz'
+body = './RyuuZeyy'
+linkf = namagh
+// WM = ./ReyToddd
+// Function ini saya bikin karena saya malas ngetik
